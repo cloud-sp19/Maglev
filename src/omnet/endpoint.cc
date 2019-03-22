ef798be4776d988532a5d08e6d21e896d8255238
Tony Yang <tonydmyang@gmail.com>
2019-03-22 13:45:06 -0400

    Style, add spacing to endpoint

--- src/omnet/endpoint.cc
+++ src/omnet/endpoint.cc
@@ -1,8 +1,15 @@
+#include <iostream>
 #include <omnetpp.h>
 
 using namespace omnetpp;
 
 class MaglevEndpoint : public cSimpleModule {
+    virtual void handleMessage(cMessage *msg) override;
 };
 
 Define_Module(MaglevEndpoint);
+
+void MaglevEndpoint::handleMessage(cMessage *msg) {
+    /* Act as a sink. */
+    delete msg;
+}
