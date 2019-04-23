b30a83d18be709cf8c46b9cb49c33ebf13ec7e20
tyang27 <tonydmyang@gmail.com>
2019-04-23 16:49:04 -0400

    Display, changed maglev to Maglev package name

--- Maglev/src/server.cc
+++ Maglev/src/server.cc
@@ -5,4 +5,10 @@ using namespace omnetpp;
 
 class Server : public cSimpleModule {
 };
+class Endpoint : public cSimpleModule {
+};
+class User : public cSimpleModule {
+};
 Define_Module(Server);
+Define_Module(Endpoint);
+Define_Module(User);
